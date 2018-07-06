0318918054fa6515ddfcd7c7e74c80aa1c81038d
Vitaliy Emelianov <vitaliy030589@gmail.com>
2018-07-06 11:16:07 +0300

    pre-release

--- resources/views/site/obrazci-dokumentov.blade.php
+++ resources/views/site/obrazci-dokumentov.blade.php
@@ -21,33 +21,36 @@
                 <tbody>
                     <tr>
                         <th scope="row">1</th>
-                        <td>Наименование документа</td>
-                        <td><a href="#">Ссылка на документ</a></td>
+                        <td>Анкета-сведения об участнике для физических лиц</td>
+                        <td><a href="{{ asset('documents/anketa_na_fizicheskoe_lico_lico_1.doc') }}">Ссылка на документ</a></td>
                     </tr>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Наименование документа</td>
-                        <td><a href="#">Ссылка на документ</a></td>
+                        <th scope="row">2</th>
+                        <td>Анкета-сведения об участнике для юридических лиц</td>
+                        <td><a href="{{ asset('documents/anketa_na_yuridicheskoe_lico_1.doc') }}">Ссылка на документ</a></td>
                     </tr>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Наименование документа</td>
-                        <td><a href="#">Ссылка на документ</a></td>
+                        <th scope="row">3</th>
+                        <td>Заявка (комиссия)
+                            <br>
+                            ПРИЛОЖЕНИЕ-1 (заявка на приобретение имущества, обращенного в собственность государства)</td>
+                        <td><a href="{{ asset('documents/zayvka_komissiya.docx') }}">Ссылка на документ</a>
+                        <br><a href="{{ asset('documents/prilozhenie-1-zayavka-na-priobretenie-imushchestva-obrashchennogo-v-sobstvennost-gosudarstva.docx') }}">Ссылка на документ</a></td>
                     </tr>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Наименование документа</td>
-                        <td><a href="#">Ссылка на документ</a></td>
+                        <th scope="row">4</th>
+                        <td>Заявка (торги)</td>
+                        <td><a href="{{ asset('documents/blank-zajavka-na-uchastie-v-torgakh.doc') }}">Ссылка на документ</a></td>
                     </tr>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Наименование документа</td>
-                        <td><a href="#">Ссылка на документ</a></td>
+                        <th scope="row">5</th>
+                        <td>Заявление на возврат задатка</td>
+                        <td><a href="{{ asset('documents/zayvlenie_vozvrat_1.doc') }}">Ссылка на документ</a></td>
                     </tr>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Наименование документа</td>
-                        <td><a href="#">Ссылка на документ</a></td>
+                        <th scope="row">6</th>
+                        <td>Опись к заявке</td>
+                        <td><a href="{{ asset('documents/opis_k_zayvke_na_torgi.doc') }}">Ссылка на документ</a></td>
                     </tr>
                 </tbody>
             </table>
