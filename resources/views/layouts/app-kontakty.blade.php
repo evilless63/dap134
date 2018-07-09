46a26ddd690db417b4ef82fa07f69dc72abfa72f
Vitaliy Emelianov <vitaliy030589@gmail.com>
2018-07-09 12:55:26 +0300

    pre-release

--- resources/views/layouts/app-kontakty.blade.php
+++ resources/views/layouts/app-kontakty.blade.php
@@ -188,6 +188,36 @@
         $('#eur_kurs').html('Курс  <i class="fa fa-usd" aria-hidden="true"></i>на текущее время: '+data.Valute.EUR.Value.toFixed(2)+' <i class="fa fa-rub" aria-hidden="true"></i>');
     });
     </script>
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter49521394 = new Ya.Metrika2({
+                        id:49521394,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true,
+                        webvisor:true
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks2");
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/49521394" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
     
 </body>
 </html>
